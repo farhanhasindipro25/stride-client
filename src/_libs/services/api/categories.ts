db893009b584581ed25ddca02d33757fedf36c87
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2024-10-30 02:11:19 +0600

    chore: Added API endpoints for categories service

--- src/_libs/services/api/categories.ts
+++ src/_libs/services/api/categories.ts
@@ -1,1 +1,27 @@
-//TODO
+import axiosInstance from "../providers/AxiosInstance";
+
+
+export const GetCategories = async (params: any) => {
+    const response = await axiosInstance.get("/categories", { params });
+    return response;
+};
+
+export const GetCategoryByUID = async (uid: string) => {
+    const response = await axiosInstance.get(`/categories/${uid}`);
+    return response;
+};
+
+export const CreateCategory = async (data: any) => {
+    const response = await axiosInstance.post("/categories/create", data);
+    return response;
+};
+
+export const UpdateCategory = async (uid: string, data: any) => {
+    const response = await axiosInstance.patch(`/categories/${uid}`, data);
+    return response;
+};
+
+export const DeleteCategory = async (uid: string) => {
+    const response = await axiosInstance.delete(`/categories/delete/${uid}`);
+    return response;
+};
